2226618e1b6967499dcdbec909183ad69e416c3f
zirra <msullivan@heysulli.com>
2020-04-20 10:56:21 -0500

    adding deploy build

---
+++ precache-manifest.c25948b0dfa8d6367c8ba51c0fbfedc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e70909edbe8c6d3038",
+    "url": "/css/app.5c9713c3.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f402b263f528ab3d6e500f8523957859",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10e70909edbe8c6d3038",
+    "url": "/js/app.566f8eb5.js"
+  },
+  {
+    "revision": "1c402cbaf8dce4524a68",
+    "url": "/js/chunk-vendors.7f2ea87c.js"
+  },
+  {
+    "revision": "22691e28293793ae0b38c9fa2a71695d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
